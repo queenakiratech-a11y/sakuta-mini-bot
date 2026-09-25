0801c749e2328250d1aeb7ef16a962b61b5d2830
queenakiratech-a11y <queenakiratech@gmail.com>
2026-09-25 10:40:46 -0400

    Update pair.js

--- api/pair.js
+++ api/pair.js
@@ -8,8 +8,8 @@
 //   SERVER2_URL = http://IP_2:PORT_2
 
 const SERVERS = {
-  1: process.env.SERVER1_URL || 'http://dobertonode.duckdns.org:3022/',
-  2: process.env.SERVER2_URL || 'https://dobertonode.duckdns.org:3002/',
+  1: process.env.SERVER1_URL || 'http://dobertonode.duckdns.org:3022',
+  2: process.env.SERVER2_URL || 'https://dobertonode.duckdns.org:3002',
 };
 
 module.exports = async (req, res) => {
